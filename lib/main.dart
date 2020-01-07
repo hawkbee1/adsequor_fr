a252e6c8674b3a576484bc74328e871bbf0ff4f7
hawkbee <googandads@gmail.com>
2020-01-07 17:49:44 +0100

    integrate links to social stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-
         primarySwatch: Colors.amber,
       ),
       home: MyHomePage(title: 'Romuald Barbe'),
@@ -29,15 +20,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -45,86 +27,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         flexibleSpace: Container(
-          decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.amber, Colors.amber[200]])),
+          decoration: BoxDecoration(
+              gradient:
+                  LinearGradient(colors: [Colors.amber, Colors.amber[200]])),
         ),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title, style: TextStyle(color: Colors.black87),),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.black87),
+        ),
       ),
       body: Center(
         child: ListView(
-
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
+            Center(
+              child: Row(
+                children: <Widget>[
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
 //              height: 50,
-                  width: 100.0,
-                  child: ClipOval(
+                      width: 100.0,
+                      child: ClipOval(
+                        child: Align(
+                            alignment: Alignment.topCenter,
+                            widthFactor: 1,
+                            heightFactor: 1,
+                            child: Image.asset('assets/profil_online.jpg')),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Align(
                       alignment: Alignment.topCenter,
-                      widthFactor: 1,
-                        heightFactor: 2,
-                        child: Image.asset('assets/profil_online.jpg')),
+                      child: Text(
+                        'Well-rounded Flutter Engineer',
+                        style: TextStyle(color: Colors.black87),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ),
+                  Spacer(),
+                ],
+              ),
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              children: [
+                FlatButton.icon(
+                  icon: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Image.asset('assets/mail.png')),
+                  label: Text('Mail'),
+                  onPressed: () async {
+                    const url = 'mailto:barbe.romuald@gmail.com';
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  }
                 ),
-                Spacer(),
+                FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset('assets/github_logo.png')),
+                    label: Text('Github'),
+                    onPressed: () async {
+                      const url = 'https://github.com/hawkbee1';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    }
+                ),
+                FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset('assets/twitter.png')),
+                    label: Text('Github'),
+                    onPressed: () async {
+                      const url = 'https://twitter.com/WhereAssistant';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    }
+                ),
+                FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset('assets/linkedin_logo.png')),
+                    label: Text('Github'),
+                    onPressed: () async {
+                      const url = 'https://www.linkedin.com/in/hawkbee/?locale=en_US';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    }
+                ),
               ],
-            ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
