fe3e5f751f15ff877ce3a95c7b78c563539fb184
Cirrus CI <support@cirruslabs.org>
2025-04-28 13:07:41 +0000

    flutter create .

--- lib/main.dart
+++ lib/main.dart
@@ -1,246 +1,122 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Welcome to adsequor.fr',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(title: 'Romuald Barbe'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    const kHtml = '<H1>soidjsoidjfoidsjf</H1></H1><iframe width="280" height="163" src="https://www.youtube.com/embed/7PxCys_X4GY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><H1>va pas ?</H1>';
-
-    final textColor = TextStyle(color: Colors.black87);
-    final textBold = TextStyle(fontWeight: FontWeight.bold);
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient:
-                  LinearGradient(colors: [Colors.amber, Colors.amber[100]])),
-        ),
-        title: Text(
-          widget.title,
-          style: textColor,
-        ),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 400),
-          child: ListView(
-            children: <Widget>[
-              Center(
-                child: Row(
-                  children: <Widget>[
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-//              height: 50,
-                        width: 100.0,
-                        child: ClipOval(
-                          child: Align(
-                              alignment: Alignment.topCenter,
-                              widthFactor: 1,
-                              heightFactor: 1,
-                              child: Image.asset('assets/profil_online.jpg')),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          'Flutter Engineer\n\nbarbe.romuald@gmail.com',
-                          style: textColor,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    Spacer(),
-                  ],
-                ),
-              ),
-              Center(
-                child: Wrap(
-                  direction: Axis.horizontal,
-                  children: [
-                    /*
-                    mailto: is not working with web interface
-
-                    FlatButton.icon(
-                        icon: SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: Image.asset('assets/mail.png')),
-                        label: Text('Mail'),
-                        onPressed: () async {
-                          const url = 'mailto:barbe.romuald@gmail.com';
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        }),
-                     */
-                    FlatButton.icon(
-                        icon: SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: Image.asset('assets/github_logo.png')),
-                        label: Text('Github'),
-                        onPressed: () async {
-                          const url = 'https://github.com/hawkbee1';
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        }),
-                    FlatButton.icon(
-                        icon: SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: Image.asset('assets/twitter.png')),
-                        label: Text('Twitter'),
-                        onPressed: () async {
-                          const url = 'https://twitter.com/WhereAssistant';
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        }),
-                    FlatButton.icon(
-                        icon: SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: Image.asset('assets/linkedin_logo.png')),
-                        label: Text('LinkedIn'),
-                        onPressed: () async {
-                          const url =
-                              'https://www.linkedin.com/in/hawkbee/?locale=en_US';
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        }),
-                  ],
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RichText(
-                      text: TextSpan(style: textColor, children: [
-                    TextSpan(
-                        text:
-                            'I am a French freelance with a Masters Degree in Engineering (computer simulation), '),
-                    TextSpan(
-                        text:
-                            'well-rounded experience in Information Technology and hands-on experience using '),
-                    TextSpan(style: textBold, text: 'Dart'),
-                    TextSpan(text: ' and '),
-                    TextSpan(style: textBold, text: 'Flutter.'),
-                  ])),
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black87,
-                            offset: new Offset(2.0, 1.0),
-                            blurRadius: 2.0,
-//                            spreadRadius: 40.0
-                        )
-                      ],
-                      border: Border.all(
-                        color: Colors.black87,
-                        width: 1.0,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        gradient: LinearGradient(
-                            colors: [Colors.amber, Colors.amber[100]])),
-                    child: InkWell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            'Download Resume',
-                            style: TextStyle(
-                                color: Colors.black87,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20.0),
-                          ),
-                        ),
-                        onTap: () async {
-                          const url =
-                              'https://drive.google.com/file/d/1dD_mQ01qFQrOioW0xoKZlH1dzZiLPpIG/view?usp=sharing';
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        }),
-                  ),
-                ),
-              ),
-//              CheckboxListTile(
-//                value: webView,
-//                onChanged: (v) => setState(() => webView = v),
-//                title: HtmlWidget('<var>.webView</var>'),
-//                subtitle: const Text('Renders web view, default ❌'),
-//              ),
-//              CheckboxListTile(
-//                value: webViewJs,
-//                onChanged: (v) => setState(() {
-//                  if (v) webView = true;
-//                  webViewJs = v;
-//                }),
-//                title: HtmlWidget('<var>.webViewJs</var>'),
-//                subtitle: const Text('Allows JavaScript execution, default ✅'),
-//              ),
-//              HtmlWidget(
-//                kHtml,
-////                key: Key("$webView$webViewJs"),
-//                unsupportedWebViewWorkaroundForIssue37: true,
-//                webView: true,
-//                webViewJs: true,
-//              ),
-            ],
-          ),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
